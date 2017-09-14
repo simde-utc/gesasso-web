ab3adce0aa97568c8544a0bfac6d9e86250eefd1
Unknown <mewenmichel@yahoo.fr>
2017-09-14 23:13:55 +0200

    Added Materialize + template pages
    Templates for home page (not logged in), search for a student, renew assocation password

---
+++ js/init.js
@@ -0,0 +1,32 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('ul:not(.collapsible) > li.active').addClass("amber darken-1");
+
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'https://placehold.it/250x250'
+        },
+        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+        onAutocomplete: function(val) {
+            // Callback function when value is autcompleted.
+        },
+        minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+    });
+
+    $('.passwd-confirm').click(function() {
+        confirmText = "announcement";
+        if ($( this ).children('i').text() == confirmText) {
+            return true;
+        } else {
+            $( this ).children('span').text("Confirmer");
+            $( this ).children('i').text(confirmText);
+            return false;
+        }
+    })
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

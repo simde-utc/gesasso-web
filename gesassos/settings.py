a658292afaa5b2594ce05d869b00bda75b93a210
Unknown <mewenmichel@yahoo.fr>
2017-09-23 13:20:59 +0200

    End merge of "mvp" and "sandbox"

--- gesassos/settings.py
+++ gesassos/settings.py
@@ -125,5 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    ("images", os.path.join(BASE_DIR, "images")),
+    ("css", os.path.join(BASE_DIR, "assets/css")),
 ]
